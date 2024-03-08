1b1e0aa9ec912b5255669145be5871b83b0663e3
Galaxy <kehkashan.sidd@gmail.com>
2024-03-08 00:25:31 -0800

    initial commit

---
+++ data_structure.js
@@ -0,0 +1,23 @@
+"use strict";
+//Data Structures in TypeScript.
+//camelCase:
+let fruitName = "Apple";
+console.log(fruitName);
+//snake_case :
+let fruit_name = "Orange";
+console.log(fruit_name);
+//PascalCase :
+let FruitName = "Mango";
+console.log(FruitName);
+//More Examples
+let isUserExist = true; /* e.g. camelCase */
+let is_user_exist = false; /* e.g. snake_case */
+let IsUserExist = 1; /* e.g. PascalCase */
+console.log(isUserExist);
+console.log(is_user_exist);
+console.log(IsUserExist);
+// MORE
+let isFeesPaid = true;
+let is_fees_paid = 'NO';
+let ISFeesPaid = 1000;
+console.log;
---
+++ data_structure.ts
@@ -0,0 +1,30 @@
+//Data Structures in TypeScript.
+
+//camelCase:
+let fruitName: string = "Apple";
+console.log(fruitName);
+
+//snake_case :
+let fruit_name: string = "Orange";
+console.log(fruit_name);
+
+//PascalCase :
+let FruitName: string = "Mango";
+console.log(FruitName);
+
+//More Examples
+
+let isUserExist = true; /* e.g. camelCase */
+let is_user_exist = false; /* e.g. snake_case */
+let IsUserExist = 1; /* e.g. PascalCase */
+
+console.log(isUserExist);
+console.log(is_user_exist);
+console.log(IsUserExist);
+
+// MORE
+let isFeesPaid : Boolean = true;
+let is_fees_paid : string = 'NO';
+let ISFeesPaid : Number = 1000;
+console.log
+
